af4baece4978c91b7fcb29d3e0ac7a1941fbe7ae
artem@stepanenko.one <artem@stepanenko.one>
2021-07-23 16:58:39 +0300

    prettier

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,7 +6,8 @@ const MIN_CORRECT_ANSWERS = 3;
 
 const isEven = (number) => number % 2 === 0;
 
-const checkAnswer = (answer, number) => (answer.toLowerCase() === 'yes' && isEven(number)) || (answer.toLowerCase() === 'no' && !isEven(number));
+const checkAnswer = (answer, number) => (answer.toLowerCase() === 'yes' && isEven(number))
+  || (answer.toLowerCase() === 'no' && !isEven(number));
 
 const main = () => {
   console.log('Welcome to the Brain Games!');
@@ -16,12 +17,14 @@ const main = () => {
   for (let step = 0; step < MIN_CORRECT_ANSWERS; step += 1) {
     const randomInt = Math.floor(Math.random() * 100);
     const userAnswer = question(`Question: ${randomInt}\n`);
-    const isCorrect = (checkAnswer(userAnswer, randomInt))
+    const isCorrect = checkAnswer(userAnswer, randomInt);
     if (isCorrect) {
       console.log('Correct!');
     } else {
       console.log('Incorrect');
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven ? 'yes' : 'no'}'.`);
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven ? 'yes' : 'no'}'.`,
+      );
       console.log("Let's try again, Bill!");
       return;
     }
