6e44f41d3d3fcce9c3211ca15d77d4598d0d3faa
Artem Stepanenko <artem@stepanenko.one>
2021-07-23 15:58:44 +0300

    realise brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+import { question } from 'readline-sync';
+import getUsername from '../src/cli.js';
+
+const MIN_CORRECT_ANSWERS = 3;
+
+const isEven = (number) => number % 2 === 0;
+
+const checkAnswer = (answer, number) => (answer.toLowerCase() === 'yes' && isEven(number)) || (answer.toLowerCase() === 'no' && !isEven(number));
+
+const main = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = getUsername();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let step = 0; step < MIN_CORRECT_ANSWERS; step += 1) {
+    const randomInt = Math.floor(Math.random() * 100);
+    const userAnswer = question(`Question: ${randomInt}\n`);
+    const isCorrect = (checkAnswer(userAnswer, randomInt))
+    if (isCorrect) {
+      console.log('Correct!');
+    } else {
+      console.log('Incorrect');
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven ? 'yes' : 'no'}'.`);
+      console.log("Let's try again, Bill!");
+      return;
+    }
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
+
+main();
--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,5 @@
 import { question } from 'readline-sync';
 
-function getUsername() {
-  const userName = question('May I have your name?\n');
-  /* eslint-disable no-console */
-  console.log('Welcome to the Brain games!');
-  console.log(`Hello, ${userName}`);
+const getUsername = () => question('May I have your name?\n');
 
-  /* eslint-enable no-console */
-}
 export default getUsername;
