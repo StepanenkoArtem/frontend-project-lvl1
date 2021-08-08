7742a82d1d524b7ec76fa3127f88e2e056e22504
artem@stepanenko.one <artem@stepanenko.one>
2021-08-08 17:42:54 +0300

    add brain-prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import { question } from 'readline-sync';
+import run from '../src/engine.js';
+import { wrongAnswerNotification } from '../src/cli.js';
+
+const TASK_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+const isPrime = (number) => {
+  for (let i = 1; i < number / 2; i += 1) {
+    if (number % 2 === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
+
+const task = () => {
+  const randomInt = Math.floor(Math.random() * 200);
+  const userAnswer = question(`Question: ${randomInt}\n`);
+  const result = isPrime(randomInt);
+  return userAnswer === result || wrongAnswerNotification(userAnswer, result);
+};
+
+const main = () => {
+  run(task, TASK_DESCRIPTION);
+};
+
+main();
