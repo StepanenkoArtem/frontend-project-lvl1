ea5c53c7cded3357fccda08fbdd6aababeeada8e
Artem Stepanenko <astepanenko@cybercraftinc.com>
2021-12-07 13:59:29 +0200

    Update engine. Remove notification function from `cli`

--- src/cli.js
+++ src/cli.js
@@ -6,9 +6,6 @@ const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const userName = getUsername();
   console.log(`Hello, ${userName}`);
-  return userName;
 };
-
-export const wrongAnswerNotification = (userAnswer, correctAnswer) => console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
 
 export default greeting;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import greeting from './cli.js';
+import { question } from 'readline-sync';
 
 const MIN_CORRECT_ANSWERS = 3;
 
 export default (task, taskDescription) => {
-  const userName = greeting();
+  console.log('Welcome to the Brain Games!');
+  const userName = question('May I have your name?\n');
+  console.log(`Hello, ${userName}`);
   console.log(taskDescription);
+
   for (let attempts = 1; attempts <= MIN_CORRECT_ANSWERS; attempts += 1) {
-    if (task()) {
+    const [userAnswer, rightAnswer] = task();
+    if (userAnswer === rightAnswer) {
       console.log('Correct');
     } else {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer}".`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
